731cf3ed5e509a6f7e48f3a32fe2d69b3b4e7993
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-02-24 12:34:44 -0500

    fix:form js

--- scripts/script.js
+++ scripts/script.js
@@ -4,31 +4,31 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__about');
 
 const editForm = document.querySelector('.form');
-
-const formInputName = editForm.querySelector('#username');
-const formInputJob = editForm.querySelector('#about');
+const form = document.querySelector('.forms');
+const formInputName = document.querySelector('#username');
+const formInputJob = document.querySelector('#about');
 const formSaveButton =document.querySelector('.form__button');
 
-
-function handleProfileFormSubmit(evt) {
-    // This line stops the browser from
-    // submitting the form in the default way.
-    evt.preventDefault();
-    // Having done so, we can define our own way of submitting the form.
-    // We'll explain it in more detail later.
-
-    // Let's find the form fields in the DOM
-    formInputName = // Use querySelector()
-    formInputJob = // Use querySelector()
-
-    // Get the values of each field from the corresponding value property
-
-    // Select elements where the field values will be entered
-
-    // Insert new values using the textContent
-    // property of the querySelector() method
+function closeForm(){
+  form.classList.remove("forms__is-open");
+}
+function openForm (){
+   form.classList.add("forms__is-open");
+   formInputName.value = profileName.textContent;
+   formInputJob.value = profileJob.textContent;
 }
 
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', handleProfileFormSubmit);
+function handleProfileFormSubmit(evt) {
+
+    evt.preventDefault();
+    profileName.textContent = formInputName.value;
+    profileJob.textContent = formInputJob.value;
+    closeForm();
+
+}
+
+
+form.addEventListener('submit', handleProfileFormSubmit);
+profileEditButton.addEventListener("click",openForm);
+editFormCloseButton.addEventListener("click", closeForm);
+
