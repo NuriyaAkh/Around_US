a057df44b04f31e235db8268bb20a99f21f02cd2
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-03-10 21:31:15 -0500

    fix everything

--- scripts/script.js
+++ scripts/script.js
@@ -17,15 +17,15 @@ const initialCards = [
   },
   {name: 'Lago di Braies', link: 'https://code.s3.yandex.net/web-code/lago.jpg'}
 ];
-// templates , may need to add .querySelector(".card"); at the end
+// templates , may need to add .querySelector(".card"); at the end ?
 const cardTemplate = document.querySelector('#card').content.querySelector(".card");
 
-// Wrappes
+// Wrappers
 const cardsContainer = document.querySelector('.cards__container');
 const editProfileForm = document.querySelector('#edit-profile');
 const addImageForm = document.querySelector('#img-add');
 const imageShowForm = document.querySelector('#image-show');
-// const form = document.querySelector(".forms");
+
 
 // buttons
 const openProfileEditButton = document.querySelector('.profile__button-name-edit');
@@ -48,58 +48,6 @@ const inputImageUrl = addImageForm.querySelector('#image-link');
 const imageTitle = imageShowForm.querySelector('.form__image-title');
 const imageElement = imageShowForm.querySelector('.form__image');
 
-
-
-// close forms
-const closeForm = (forms) => {
-  forms.classList.remove('forms_is-open');
-};
-function closeAddImageForm() {
-  addImageForm.classList.remove('forms_is-open');
-}
-function closeShowImageForm() {
-  imageShowForm.classList.remove('forms_is-open');
-}
-
-
-// function open forms need to one function for all 3 forms
-function openForm() {
-  editProfileForm.classList.add('forms_is-open');
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
-}
-//need help
-function openAddImageForm() {
-  addImageForm.classList.add('forms_is-open');
-  inputImagePlaceName.value = imageTitle.textContent;
-}
-
-function openImageForm() {
-  imageShowForm.classList.add('forms_is-open');
-  const formImage = evt.target.src;
-  const formImageTitle = evt.target.alt;
-}
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileJob.textContent = inputJob.value;
-  closeForm();
-}
-
-//need help with handleAddImageFormSubmit
-function handleAddImageFormSubmit(evt) {
-  evt.preventDefault();
-  imageTitle.textContent = InputImagePlace.value;
-  closeAddImageForm();
-}
-
-editProfileForm.addEventListener('submit', handleProfileFormSubmit);
-openProfileEditButton.addEventListener('click', openForm);
-closeEditFormButton.addEventListener('click', () =>{ closeForm(editProfileForm); });
-addImageForm.addEventListener('submit', handleAddImageFormSubmit);
-openImageAddButton.addEventListener('click', openAddImageForm);
-closeAddImageFormButton.addEventListener('click', () => {closeForm(addImageForm);});
 // card add
 function createCard (data) {
   const cardElement = cardTemplate.cloneNode(true);
@@ -120,6 +68,50 @@ function createCard (data) {
   cardImage.addEventListener('click', () => handleShowImage(data));
   return cardElement;
 }
+// close forms
+const closeForm = (forms) => {
+  forms.classList.remove('forms_is-open');
+};
+//open forms
+function openForm (forms){
+  forms.classList.add('forms_is-open');
+}
+//need help
+function openAddImageForm() {
+ addImageForm.classList.add('forms_is-open');
+ inputImagePlaceName.value = imageTitle.textContent;
+ openForm(addImageForm);
+}
+
+function openImageShowForm() {
+
+  const formImage = evt.target.src;
+  const formImageTitle = evt.target.alt;
+  openForm(imageShowForm);
+}
+
+// function open edit forms with preselected old input
+function openEditProfileForm() {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+   openForm(editProfileForm);
+ }
+//function to submit edit profile info,checks the data is entered
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  closeForm(editProfileForm);
+}
+
+editProfileForm.addEventListener('submit', handleProfileFormSubmit);
+openProfileEditButton.addEventListener('click', openEditProfileForm);
+addImageForm.addEventListener('submit', handleImageFormSubmit);
+openImageAddButton.addEventListener('click', openAddImageForm);
+closeEditFormButton.addEventListener('click', () =>{closeForm(editProfileForm); });
+closeAddImageFormButton.addEventListener('click', () => {closeForm(addImageForm);});
+closeShowImageButton.addEventListener('click', ()=>{closeForm(imageShowForm);});
+
 // delete card function
 function handleDeleteCard(evt) {
   evt.target.closest('.card').remove();
@@ -128,26 +120,29 @@ function handleDeleteCard(evt) {
 function handleLikeButton(evt){
   evt.target.classList.toggle('card__button_active');
 }
+// function show image
 function handleShowImage(data){
   imageElement.src = data.link;
   imageElement.alt = `${data.name}`;
-  imageName.textContent = data.name;
+  imageTitle.textContent = data.name;
   openForm(imageShowForm);
 }
+//function to submit and check for required fields add image form
 function handleImageFormSubmit(evt){
   evt.preventDefault();
   renderCard(
-    { name:cardNameInput.value,
-      link: cardLinkInput.value,
+    { name:inputImagePlaceName.value,
+      link: inputImageUrl.value,
   },
   cardsContainer );
-  closeForm(imageAddForm);
+  closeForm(addImageForm);
 }
-function renderCard(data, wrap){
-  wrap.preend(createCard(data));
+//render
+function renderCard(data, cardsContainer){
+  cardsContainer.prepend(createCard(data));
 
 };
-//render
+// generate 6 cards
 initialCards.forEach((data) => {
-  rendercard(data,cardsContainer);
+  renderCard(data, cardsContainer);
 });
