cd36932e02e170aa7ced88132ba086d15592f77d
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-03-19 18:12:25 -0400

    feature form button state/still need work

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,7 +5,7 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-input-error`);
   inputElement.classList.add("form__input_type_error");
-  //console.log(`.${inputElement.id}-input-error`);
+  //console.log(`.${inputElement.id}-input-error`); checking
   errorElement.textContent = errorMessage;
   errorElement.classList.add("form__error-text_active");
 };
@@ -30,11 +30,16 @@ const setEventListeners = (formElement) =>{
    // Find all fields inside the form, and
   // make an array from them using the Array.from() method
   const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const buttonElement = formElement.querySelector(".form__button");
+  // Call the toggleButtonState()
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input",() =>{
       // Call the isValid() function inside the callback,
       // and pass the form and the element to be checked to it
-      isValid(formElement,inputElement)
+      isValid(formElement,inputElement);
+      toggleButtonState(inputList, buttonElement);
+
     });
   });
 };
@@ -59,4 +64,30 @@ enableValidation({
   inputErrorClass: ".form__input_type_error",
   errorClass: ".form__error-text_active"
 });
+//button state feature
 
+// The function takes an array of fields
+
+const hasInvalidInput = (inputList) => {
+  // iterate over the array using the some() method
+  return inputList.some((inputElement) => {
+        // If the field is invalid, the callback will return true.
+    // The method will then stop, and hasInvalidInput() function will return true
+    // hasInvalidInput returns true
+
+    return !inputElement.validity.valid;
+  })
+};
+// The function takes an array of input fields
+// and the button element, whose state you need to change
+
+const toggleButtonState = (inputList, buttonElement) => {
+  // If there is at least one invalid input
+  if (hasInvalidInput(inputList)) {
+    // make the button inactive
+    buttonElement.classList.add("form__submit_disabled");
+  } else {
+        // otherwise, make it active
+    buttonElement.classList.remove("form__submit_disabled");
+  }
+};
