7cca5c962614025a0bf0b5b4ee9f4084589a6cdd
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-03-31 22:20:18 -0400

    feature Card class, handle like button, delete card, todo show big image

--- scripts/script.js
+++ scripts/script.js
@@ -26,6 +26,84 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
+const popupImage = document.querySelector(".forms__image");
+const popupImageTitle = document.querySelector(".forms__image-title");
+const popupShowImageElement = document.querySelector("#image-show");
+const popupCloseButton = document.querySelector(".forms__button-close");
+const popupElement = document.querySelector(".forms");
+class Card {
+  constructor(data){
+    this._name = data.name;
+    this._link = data.link;
+
+  }
+  _getTemplate() {
+  const cardElement = document
+  .querySelector("#card")
+  .content.querySelector(".card").cloneNode(true);
+
+  return cardElement;
+  }
+  generateCard() {
+    // Store the markup in the private field _element
+  // so that other elements can access it
+    this._element = this._getTemplate();
+    this._setEventListeners();
+    // Add data
+    this._element.querySelector(".card__title").textContent = this._name;
+    this._element.querySelector(".card__img").src = this._link;
+   // Return the element
+   return this._element;
+
+  }
+  _setEventListeners(){
+    //like button
+    this._element.querySelector(".card__button").addEventListener("click",(evt) => {
+      this._handleLikeButton(evt);
+    });
+    //open Show Image popup
+    this._element.querySelector(".card__img").addEventListener("click",() => {
+      this._handleShowImage();
+    });
+    //delete card
+    this._element.querySelector(".card__delete").addEventListener("click",(evt) => {
+      this._handleDeleteCard(evt);
+    });
+  }
+
+  _handleShowImage (){
+    popupImage.src = this._link;
+    popupImage.alt = `${this._name}`;
+    popupImageTitle.textContent = this._name;
+    openPopup(popupShowImageElement);
+
+  }
+
+  _handleLikeButton(evt) {
+    evt.target.classList.toggle("card__button_active");
+  }
+
+  _handleDeleteCard(evt) {
+    evt.target.closest(".card").remove();
+}
+}
+// iterate over the entire original array
+initialCards.forEach((item) => {
+ // Create a card instance
+ const card = new Card (item); // pass an object as an argument
+ // Fill up the card and return it
+ const cardElement = card.generateCard();
+  // Add it to the DOM
+  document.querySelector(".cards__container").prepend(cardElement);
+});
+//open forms
+function openPopup(popup) {
+  popup.classList.add("forms_is-open");
+  document.addEventListener("keydown",handleKeyEsc);
+  document.addEventListener("mousedown",handleMouseDown);
+}
+
+/*
 // templates
 const cardTemplate = document
   .querySelector("#card")
@@ -194,3 +272,4 @@ function handleMouseDown(evt){
       closePopup(currentPopup);
     }
 };
+*/
