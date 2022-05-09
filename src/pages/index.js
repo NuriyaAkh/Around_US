01767ebbc45fc09d99325c74cd92c8e3042a4794
Nuriya Akhmedova <nuriya_a@yahoo.com>
2022-05-09 18:45:10 -0400

    ащкьфештп

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ import {
   userJobInput,
 } from "../scripts/utils/constants.js";
 
-
 let cardList;
 let currentUserId;
 const api = new Api({
@@ -48,33 +47,29 @@ editProfileFormValidator.enableValidation();
 addImageFormValidator.enableValidation();
 editProfilePictureFormValidator.enableValidation();
 //init cards, user info
-api.promiseAll()
-.then(([user,cardData]) => {
-  profileInfo.setUserInfo({
-    userInputName: user.name,
-    userInputJob: user.about,
-    userAvatar: user.avatar,});
-
-  currentUserId = user._id;
-  cardList = new Section(
-    {
+api
+  .promiseAll()
+  .then(([user, cardData]) => {
+    profileInfo.setUserInfo({
+      userInputName: user.name,
+      userInputJob: user.about,
+      userAvatar: user.avatar,
+    });
+    currentUserId = user._id;
+    cardList = new Section({
       items: cardData,
       renderer: renderCard,
-      containerSelector: ".cards__container",}
-  );
-
-  cardList.renderItems();
-
-})
-.catch(err => console.error(`Error while executing: ${err}`));
+      containerSelector: ".cards__container",
+    });
+    cardList.renderItems();
+  })
+  .catch((err) => console.error(`Error while executing: ${err}`));
 
 //init popup add new image *
 const addNewImageForm = new PopupWithForm({
   popupSelector: "#img-add",
   handleFormSubmit: (data) => {
-     api.addNewCard(data)
-    .then(data =>
-    renderCard(data));
+    api.addNewCard(data).then((data) => renderCard(data));
   },
 });
 addNewImageForm.setEventListeners();
@@ -104,72 +99,58 @@ const cardShowImage = new PopupWithImage("#image-show");
 
 //init confirmation popup to delete card
 const deleteConfirmationForm = new PopupWithConfirmation({
-  popupSelector:"#confirm-popup"});
+  popupSelector: "#confirm-popup",
+});
 
 // render card
 function renderCard(data) {
-
-  const card = new Card(
-    {
-      data,
-      handleImageClick: () => {
-        cardShowImage.open(data);
-      },
-      handleTrashClick: () => {
-        deleteConfirmationForm.setSubmit(() => {
-          console.log(card.getCardId());
-          api
-            .deleteCard(card.getCardId())
-            .then((res) => {
-              deleteConfirmationForm.close();
-              card.handleDeleteCard();
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        });
-        deleteConfirmationForm.open();
-
-      },
-      handleLikeClick: (card) => {
-
-          if (card.isLiked()) {
-            api
-              .removeLike(card.getCardId())
-
-                .then((res) =>
-        card.handleLikeData(res))
-        .catch((err) => {
-          console.log(err);
-        });
-      }
-      else {
+  const card = new Card({
+    data,
+    handleImageClick: () => {
+      cardShowImage.open(data);
+    },
+    handleTrashClick: () => {
+      deleteConfirmationForm.setSubmit(() => {
+        console.log(card.getCardId());
         api
-              .addLike(card.getCardId())
-
-                .then((res) =>
-        card.handleLikeData(res))
-        .catch((err) => {
-          console.log(err);
-        });
-
-
+          .deleteCard(card.getCardId())
+          .then((res) => {
+            deleteConfirmationForm.close();
+            card.handleDeleteCard();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+      deleteConfirmationForm.open();
+    },
+    handleLikeClick: (card) => {
+      if (card.isLiked()) {
+        api
+          .removeLike(card.getCardId())
+          .then((res) => card.handleLikeData(res))
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        api
+          .addLike(card.getCardId())
+          .then((res) => card.handleLikeData(res))
+          .catch((err) => {
+            console.log(err);
+          });
       }
     },
-      currentUserId,
-      cardSelector: "#card",
-    },
-
-  );
+    currentUserId,
+    cardSelector: "#card",
+  });
   const cardElement = card.generateCard();
- // console.log(card);
+  // console.log(card);
   cardList.addItem(cardElement);
 }
 
-
 // function show add image form
 function openAddImageForm() {
-  imageForm.reset(); // reset form
   addImageFormValidator.resetValidation();
   addNewImageForm.open();
 }
@@ -178,7 +159,6 @@ function openAddImageForm() {
 function openEditProfileForm() {
   fillProfileForm();
   editProfileFormValidator.resetValidation();
-
   editUserInfoForm.open();
 }
 // prefill the profile form
@@ -187,49 +167,48 @@ function fillProfileForm() {
   userNameInput.value = userInputName;
   userJobInput.value = userInputJob;
 }
-function openEditProfilePictureForm(){
-  avatarForm.reset();
+function openEditProfilePictureForm() {
   editProfilePictureFormValidator.resetValidation();
   editUserImageForm.open();
 }
 // function to submit edit profile info
 function handleProfileFormSubmit(userData) {
-editUserInfoForm.renderLoading(true);
-  api.editProfileInfo(userData)
+  editUserInfoForm.renderLoading(true);
+  api
+    .editProfileInfo(userData)
     .then((userData) => {
       console.log(userData);
       profileInfo.setUserInfo({
         userInputName: userData.name,
         userInputJob: userData.about,
-        userAvatar: userData.avatar
+        userAvatar: userData.avatar,
       });
-
     })
     .catch((err) => {
       console.log(err); // log the error to the console
     })
-    .finally(()=>{
+    .finally(() => {
       editUserInfoForm.renderLoading(false);
     });
-  }
-  //function to handle editUserImageForm
-function handleProfileImageSubmit(data){
+}
+//function to handle editUserImageForm
+function handleProfileImageSubmit(data) {
   editUserImageForm.renderLoading(true);
-  api.editProfilePicture(data)
-  .then((result)=> {
-    profileInfo.setUserInfo({
-      userInputName: result.name,
-      userInputJob: result.about,
-      userAvatar: result.avatar
+  api
+    .editProfilePicture(data)
+    .then((result) => {
+      profileInfo.setUserInfo({
+        userInputName: result.name,
+        userInputJob: result.about,
+        userAvatar: result.avatar,
+      });
+    })
+    .catch((err) => {
+      console.log(err); // log the error to the console
+    })
+    .finally(() => {
+      editUserImageForm.renderLoading(false);
     });
-
-  })
-  .catch((err) => {
-    console.log(err); // log the error to the console
-  })
-  .finally(()=>{
-    editUserImageForm.renderLoading(false)
-  })
 }
 
 // event listnerens
@@ -238,36 +217,3 @@ openImageAddButton.addEventListener("click", openAddImageForm);
 editProfilePictureButton.addEventListener("click", openEditProfilePictureForm);
 
 
-//  fetch("https://around.nomoreparties.co/v1/group-12/cards", {
-//   headers: {
-//     authorization: "66d060c3-a92b-49d0-add5-d7e29bf411c9"
-//   }
-// })
-//   .then(res => res.json())
-//   .then((result) => {
-//     console.log(result);
-//   });
-//   fetch ("https://around.nomoreparties.co/v1/group-12/users/me", {
-//   headers: {
-//     authorization: "66d060c3-a92b-49d0-add5-d7e29bf411c9"
-//   }
-// })
-//   .then(res => res.json())
-//   .then((result) => {
-//     console.log(result);
-//   });
-/* fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
-  method: "PATCH",
-  headers: {
-    authorization: "66d060c3-a92b-49d0-add5-d7e29bf411c9",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: "Nuriya Akhmedova",
-    about: "Software Developer, explorer, mom"
-  })
-}); */
-
-//toDo
-
-//likes function
